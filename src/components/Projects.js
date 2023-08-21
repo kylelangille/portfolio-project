cbd05f0592fb0f8d45f033507098f98b7b5d6ee1
Kyle Langille <kyle.a.langille@gmail.com>
2023-08-21 07:56:40 -0230

    exported projectData into its own file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,37 +3,9 @@ import Wrapper from "./UI/Wrapper";
 import Heading from "./UI/Heading";
 import ProjectCard from "./UI/ProjectCard";
 import Content from "./UI/Content";
+import { projectData } from "../projectData";
 
 const Projects = () => {
-  const projectData = [
-    {
-      title: "Portfolio Page",
-      description: "My portfolio page. Made with React and JavaScript.",
-      src: "port.png",
-      alt: "A screenshot of Kyle Langille's portfolio page",
-      repoLink: "https://github.com/kylelangille/portfolio-project",
-      demoLink: "https://google.com",
-    },
-    {
-      title: "Fake Windows XP",
-      description:
-        "A recreation of Windows XP in your browser. Made with HTML, CSS, and vanilla JavaScript.",
-      src: "fwin.png",
-      alt: "A screenshot of Fake Windows XP",
-      repoLink: "https://github.com/kylelangille/windows-xp-v1",
-      demoLink: "https://fwindows-xp.netlify.app/",
-    },
-    {
-      title: "Nietzsche Quote Generator",
-      description:
-        "A quote generator for Friedrich Nietzsche. Made with HTML, CSS, and vanilla JavaScript.",
-      src: "quote-generator.png",
-      alt: "A screenshot of Nietzsche Quote Generator",
-      repoLink: "https://github.com/kylelangille/nietzsche-quote-generator",
-      demoLink: "https://kylelangille.github.io/nietzsche-quote-generator/",
-    },
-  ];
-
   return (
     <Wrapper id="projects">
       <Heading>Projects</Heading>
---
+++ src/projectData.js
@@ -0,0 +1,28 @@
+export const projectData = [
+  {
+    title: "Portfolio Page",
+    description: "My portfolio page. Made with React and JavaScript.",
+    src: "port.png",
+    alt: "A screenshot of Kyle Langille's portfolio page",
+    repoLink: "https://github.com/kylelangille/portfolio-project",
+    demoLink: "https://google.com",
+  },
+  {
+    title: "Fake Windows XP",
+    description:
+      "A recreation of Windows XP in your browser. Made with HTML, CSS, and vanilla JavaScript.",
+    src: "fwin.png",
+    alt: "A screenshot of Fake Windows XP",
+    repoLink: "https://github.com/kylelangille/windows-xp-v1",
+    demoLink: "https://fwindows-xp.netlify.app/",
+  },
+  {
+    title: "Nietzsche Quote Generator",
+    description:
+      "A quote generator for Friedrich Nietzsche. Made with HTML, CSS, and vanilla JavaScript.",
+    src: "quote-generator.png",
+    alt: "A screenshot of Nietzsche Quote Generator",
+    repoLink: "https://github.com/kylelangille/nietzsche-quote-generator",
+    demoLink: "https://kylelangille.github.io/nietzsche-quote-generator/",
+  },
+];
