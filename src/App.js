d086352d15ac8f87b638f468911b635aea19ce6a
Kyle Langille <103784140+kylelangille@users.noreply.github.com>
2025-03-10 14:09:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import { useState, useEffect } from "react";
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./components/Styles/GlobalStyles";
-import { darkTheme, lightTheme } from "./components/Styles/Themes";
-import Header from "./components/Sections/Header";
-import Hero from "./components/Sections/Hero";
-import About from "./components/Sections/About";
-import Projects from "./components/Sections/Projects";
-import Contact from "./components/Sections/Contact";
-import Footer from "./components/Sections/Footer";
+// import { useState, useEffect } from "react";
+// import { ThemeProvider } from "styled-components";
+// import GlobalStyles from "./components/Styles/GlobalStyles";
+// import { darkTheme, lightTheme } from "./components/Styles/Themes";
+// import Header from "./components/Sections/Header";
+// import Hero from "./components/Sections/Hero";
+// import About from "./components/Sections/About";
+// import Projects from "./components/Sections/Projects";
+// import Contact from "./components/Sections/Contact";
+// import Footer from "./components/Sections/Footer";
 
 const App = () => {
-  const [theme, setTheme] = useState("dark");
-  const [animationStarted, setAnimationStarted] = useState(false);
+  // const [theme, setTheme] = useState("dark");
+  // const [animationStarted, setAnimationStarted] = useState(false);
 
-  const themeHandler = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  };
+  // const themeHandler = () => {
+  //   theme === "light" ? setTheme("dark") : setTheme("light");
+  // };
 
-  useEffect(() => {
-    const animationTimeout = setTimeout(() => {
-      setAnimationStarted(true);
-    }, 200);
+  // useEffect(() => {
+  //   const animationTimeout = setTimeout(() => {
+  //     setAnimationStarted(true);
+  //   }, 200);
 
-    return () => clearTimeout(animationTimeout);
-  }, [animationStarted]);
+  //   return () => clearTimeout(animationTimeout);
+  // }, [animationStarted]);
 
   return (
     // <>
